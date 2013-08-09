7eafb4e6c270d3db7f489325547b939715e54471
Rafael Oliveira <rafaelbco@gmail.com>
2013-08-09 16:01:36 -0300

    Updates.

--- bash/net/amazon_login.sh
+++ bash/net/amazon_login.sh
@@ -1,2 +1,2 @@
-ssh -i ~/.ssh/amazon.pem ubuntu@$AMAZON_HOST
+ssh -i ~/.ssh/amazon.pem -p $AMAZON_PORT ubuntu@$AMAZON_HOST
 
--- bash/net/amazon_socks.sh
+++ bash/net/amazon_socks.sh
@@ -1,1 +1,1 @@
-ssh -i ~/.ssh/amazon.pem -NnC2Tv -D 8087 ubuntu@$AMAZON_HOST
+ssh -i ~/.ssh/amazon.pem -NnC2Tv -D 8087 -p $AMAZON_PORT ubuntu@$AMAZON_HOST
