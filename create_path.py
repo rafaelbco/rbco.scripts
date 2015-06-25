00a54404547916f4246fa3c98894f66a7666b50d
Rafael Oliveira <rafaelbco@gmail.com>
2015-06-25 08:12:18 -0300

    Update create_path

--- create_path.py
+++ create_path.py
@@ -8,4 +8,7 @@ if __name__ == '__main__':
     directories = [os.path.join(base_path, d) for d in os.listdir(base_path)]
     directories = [d for d in directories if os.path.isdir(d)]
 
+
+
     print 'export PATH=' + ':\\\n'.join(['$PATH'] + directories)
+
