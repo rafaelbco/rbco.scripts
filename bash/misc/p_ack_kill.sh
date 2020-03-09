9bc6157f44c06c3ea19dd90b423ffeaccf4e58d3
Rafael Oliveira <rafaelbco@gmail.com>
2020-03-09 11:31:14 -0300

    Remove deprecated scripts

--- bash/misc/p_ack_kill.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ack_ps "" | percol | tr -s ' ' | cut -d' '  -f2 | xargs kill $2 $3 $4 $5 $6 $7
--- bash/net/http_headers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Usage: http_headers.sh [wget_options] <url>
-wget -q -S -O /dev/null $@
